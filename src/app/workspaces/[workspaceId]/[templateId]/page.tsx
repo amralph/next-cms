6eed350f15ae3928ae048aa1e3085e6d5f2c5a41
unknown <rs1917@hotmail.com>
2025-12-27 03:49:45 -0500

    load initial referencable documents

--- src/app/workspaces/[workspaceId]/[templateId]/page.tsx
+++ src/app/workspaces/[workspaceId]/[templateId]/page.tsx
@@ -2,9 +2,10 @@ import { DocumentsClient } from './DocumentsClient';
 import { addSignedContentToDocuments } from './signDocument';
 import { getUserOrRedirect } from '@/lib/getUserOrRedirect';
 import { createClient } from '@/lib/supabase/server';
-import { SignedDocumentRow, StorageObject } from '@/types/types';
+import { DocumentRow, SignedDocumentRow, StorageObject } from '@/types/types';
 import { signUrlsAndExtractData } from '../settings/page';
 import { initialPage, pageSize } from '@/lib/pagination';
+import { SupabaseClient } from '@supabase/supabase-js';
 
 const page = async ({
   params,
@@ -31,7 +32,7 @@ const page = async ({
     .eq('id', templateId)
     .single();
 
-  // Get documents in the workspace
+  // Get documents in the workspace that are part of this template
   const { data: documents } = await supabase
     .from('documents')
     .select('*')
@@ -42,7 +43,6 @@ const page = async ({
   if (documents) await addSignedContentToDocuments(documents);
 
   // get files
-
   const bucket = process.env.SUPABASE_BUCKET!;
 
   const storageObjects = (
@@ -60,6 +60,44 @@ const page = async ({
     bucket!
   );
 
+  // get first references here
+  // get all reference types from the template
+  // put them into a set because we only need one of them
+  // if reference id doesn't exist in the set, add the documents to the set.
+  // set will be indexed by id
+
+  // then handle front end
+
+  // in each field, if type === reference OR if type === array and arrayOf === reference,
+  //   add the items of referenceTo to the set
+  //
+  // once we have the referenceIds, get the first n documents of each from supabase.
+
+  // collect referenceIds
+
+  const referenceIds = new Set<string>();
+
+  for (const field of template?.template.fields) {
+    if (
+      field.type === 'reference' ||
+      (field.type === 'array' && field.arrayOf === 'reference')
+    ) {
+      for (const refId of field.referenceTo ?? []) {
+        referenceIds.add(refId);
+      }
+    }
+  }
+
+  const initialReferencableDocuments = await fetchInitialtReferencableDocuments(
+    referenceIds,
+    pageSize,
+    supabase
+  );
+
+  // handle this on front end by templateId
+
+  // let's possibly refactor without prop drilling
+
   return (
     <DocumentsClient
       documents={documents as SignedDocumentRow[]}
@@ -68,8 +106,34 @@ const page = async ({
       workspaceId={workspaceId}
       workspaceName={workspace?.name}
       initialFiles={signedFiles}
+      initialReferencableDocuments={initialReferencableDocuments}
     />
   );
 };
 
 export default page;
+
+async function fetchInitialtReferencableDocuments(
+  templateIds: Set<string>,
+  pageSize: number,
+  supabase: SupabaseClient
+) {
+  const results: Record<string, DocumentRow[]> = {};
+
+  for (const templateId of templateIds) {
+    const { data, error } = await supabase
+      .from('documents')
+      .select('*')
+      .eq('template_id', templateId)
+      .order('created_at', { ascending: true })
+      .limit(pageSize);
+
+    if (error) {
+      throw error;
+    }
+
+    results[templateId] = data ?? [];
+  }
+
+  return results;
+}
